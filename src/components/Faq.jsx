2288e133b09ffeb17431b7ef57df195a40c2d2a6
NaraeJang <j.naaaarae@gmail.com>
2024-03-10 18:47:24 -0400

    update faq mobile

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,12 +1,20 @@
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 import { FaqQuestion, Title } from './';
 import { faqQuestions } from '../data/data';
 
 const FaqContext = createContext();
 
 const Faq = () => {
+  const [questions, setQuestions] = useState(faqQuestions);
+  const [activeId, setActiveId] = useState(null);
+
+  const toggleQuestion = (id) => {
+    const newActiveId = id === activeId ? null : id;
+    setActiveId(newActiveId);
+  };
+
   return (
-    <FaqContext.Provider value={{}}>
+    <FaqContext.Provider value={{ toggleQuestion, questions, activeId }}>
       <section id="faq">
         <div className="custom-container">
           <div className="faq-container">
--- src/components/FaqQuestion.jsx
+++ src/components/FaqQuestion.jsx
@@ -1,4 +1,65 @@
-const FaqQuestion = () => {
-  return <div>FaqQuestion</div>;
+import { useRef, useState, useEffect } from 'react';
+import { useFaqContext } from './Faq';
+
+const FaqQuestion = ({ id, title, info }) => {
+  const { activeId, toggleQuestion } = useFaqContext();
+
+  const isActive = id === activeId;
+
+  const infoContainerRef = useRef(null);
+  const infoRef = useRef(null);
+  const [infoHeight, setInfoHeight] = useState(0);
+
+  useEffect(() => {
+    if (infoRef.current) {
+      setInfoHeight(
+        isActive ? infoRef.current.getBoundingClientRect().height : 0
+      );
+    }
+  }, [isActive]);
+
+  return (
+    <article className="faq-container">
+      <header className="p-4">
+        <button
+          type="button"
+          className="flex flex-row justify-between items-center  w-full"
+          onClick={() => {
+            toggleQuestion(id);
+            console.log(
+              'containter',
+              infoContainerRef.current.getBoundingClientRect()
+            );
+            console.log(infoRef.current.getBoundingClientRect());
+
+            console.log(isActive);
+          }}>
+          <h5 className="text-left poppins-medium leading-relaxed ">{title}</h5>
+          <div
+            className={
+              isActive
+                ? 'plus-minus question-active shrink-0'
+                : 'plus-minus shrink-0'
+            }></div>
+        </button>
+      </header>
+      <hr />
+      <div
+        className="info-container"
+        ref={infoContainerRef}
+        style={{
+          height: `${infoHeight}px`,
+        }}>
+        <div className="infos" ref={infoRef}>
+          {isActive && (
+            <>
+              <p className="px-4 pt-6 pb-10 text-left">{info}</p>
+              <hr />
+            </>
+          )}
+        </div>
+      </div>
+    </article>
+  );
 };
 export default FaqQuestion;
