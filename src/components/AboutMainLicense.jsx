dcd3bea5eeb1d018e6f6e75d31392be05ebe50a2
NaraeJang <j.naaaarae@gmail.com>
2024-03-11 14:32:30 -0400

    complete about-license section

--- src/components/AboutMainLicense.jsx
+++ src/components/AboutMainLicense.jsx
@@ -1,4 +1,14 @@
+import { certificationCards } from '../data/data';
+import { Title, AboutCards } from './';
+
 const AboutMainLicense = () => {
-  return <div>AboutMainLicense</div>;
+  return (
+    <section id="about-license">
+      <div className="custom-container">
+        <Title title="certifications" />
+        <AboutCards cards={certificationCards} />
+      </div>
+    </section>
+  );
 };
 export default AboutMainLicense;
--- src/data/data.js
+++ src/data/data.js
@@ -141,5 +141,68 @@ graphic design`,
 ];
 
 export const certificationCards = [
-  { id: nanoid(), year: ``, title: ``, linkTitle: ``, link: `` },
+  {
+    id: nanoid(),
+    year: `2023 | Udemy`,
+    title: `Advanced CSS and Sass: Flexbox, Grid, Animations and More!`,
+    linkTitle: `Show credential`,
+    link: `https://www.udemy.com/certificate/UC-535ee444-4dde-42a3-9dfb-34b17820c79d/`,
+  },
+  {
+    id: nanoid(),
+    year: `2023 | Udemy`,
+    title: `Javascript Course`,
+    linkTitle: `Show credential`,
+    link: `https://www.udemy.com/certificate/UC-31894b51-6924-4b1f-9268-23673cb0e005/`,
+  },
+  {
+    id: nanoid(),
+    year: `2023 | Udemy`,
+    title: `JavaScript: The Advanced Concepts`,
+    linkTitle: `Show credential`,
+    link: `https://www.udemy.com/certificate/UC-83043455-82ef-4395-aed4-e6743bbfd5ed/`,
+  },
+  {
+    id: nanoid(),
+    year: `2023 | Udemy`,
+    title: `Modern HTML & CSS (Including Sass)`,
+    linkTitle: `Show credential`,
+    link: `https://www.udemy.com/certificate/UC-0569ec06-1f3c-4310-898c-78732f1c71d1/`,
+  },
+  {
+    id: nanoid(),
+    year: `2023 | Udemy`,
+    title: `The Complete 2023 Web Development Bootcamp`,
+    linkTitle: `Show credential`,
+    link: `https://www.udemy.com/certificate/UC-f800feed-80ba-46e0-a5f2-689df0dbb0a4/`,
+  },
+  {
+    id: nanoid(),
+    year: `2022 | Coursera`,
+    title: ` Google UX Design Specialization`,
+    linkTitle: `Show credential`,
+    link: `https://www.coursera.org/account/accomplishments/specialization/certificate/YX8EV7DBR75N`,
+  },
+  {
+    id: nanoid(),
+    year: `2023 / freeCodeCamp`,
+    title: `Responsive Web Design`,
+    linkTitle: `Show credential`,
+    link: `https://www.freecodecamp.org/certification/NaraeJang/responsive-web-design`,
+  },
+  {
+    id: nanoid(),
+    year: `2022 | Coursera`,
+    title: `Complete Web&Mobile Design: UI/UX, Figma`,
+    linkTitle: `Show credential`,
+    link: `https://www.udemy.com/certificate/UC-2c574869-b5a0-4892-8800-75a420a0e4be/`,
+  },
 ];
+
+// {
+//   id: nanoid(),
+//   year: `2023 | Udemy`,
+//   title: ``,
+//   linkTitle: `Show credential`,
+//   link: ``,
+// },
