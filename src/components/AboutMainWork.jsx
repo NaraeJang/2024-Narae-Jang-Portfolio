f0c498004dd75791040b773c04f586ce6b327bab
NaraeJang <j.naaaarae@gmail.com>
2024-03-11 11:34:39 -0400

    complete mobile ver about-work section

--- src/components/AboutMainWork.jsx
+++ src/components/AboutMainWork.jsx
@@ -1,4 +1,35 @@
+import { workExperience } from '../data/data';
+import { Title } from './';
+
 const AboutMainWork = () => {
-  return <div>AboutMainWork</div>;
+  return (
+    <section id="about-work">
+      <div className="custom-container">
+        <Title title="work experience" />
+        <div className="work-list text-left mt-6">
+          {workExperience.map((work) => {
+            return (
+              <article key={work.id}>
+                <span className="text-sm">{work.year}</span>
+                <h4 className="text-lg poppins-medium leading-loose">
+                  {work.title}
+                </h4>
+                {work.company && <p className="text-sm">{work.company}</p>}
+                {work.other && (
+                  <div>
+                    <h5 className="text-sm poppins-medium pb-[2px]">
+                      Past Clients:
+                    </h5>
+                    <p className="text-sm">{work.other.clients}</p>
+                  </div>
+                )}
+                <hr className="my-8" />
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 export default AboutMainWork;
--- src/data/data.js
+++ src/data/data.js
@@ -92,3 +92,31 @@ export const faqQuestions = [
     info: `I'm korean.`,
   },
 ];
+
+// ABOUT PAGE
+export const workExperience = [
+  {
+    id: nanoid(),
+    title: 'UX/UI Designer ❘ Senior Graphic Designer',
+    company: 'Calego Inc.',
+    year: '2022',
+    other: null,
+  },
+  {
+    id: nanoid(),
+    title: 'Web & Graphic Designer',
+    company: 'Group Gabriel',
+    year: '2019 - 2021',
+    other: null,
+  },
+  {
+    id: nanoid(),
+    title: 'Freelance Designer',
+    company: null,
+    year: '2018 - 2023',
+    other: {
+      clients:
+        'EW Network, R&K consulting, bar ganadara, jako restaurant, han international, wegic',
+    },
+  },
+];
