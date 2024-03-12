20e4e163c2228f85c406e537d04f6b359656d2a4
NaraeJang <j.naaaarae@gmail.com>
2024-03-12 18:08:58 -0400

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,32 @@ const Header = () => {
 
   const isPageLoading = navigation.state === 'loading';
 
+  document.addEventListener('DOMContentLoaded', () => {
+    const interBubble = document.querySelector('.interactive');
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    function move() {
+      curX += (tgX - curX) / 20;
+      curY += (tgY - curY) / 20;
+      interBubble.style.transform = `translate(${Math.round(
+        curX
+      )}px, ${Math.round(curY)}px)`;
+      requestAnimationFrame(() => {
+        move();
+      });
+    }
+
+    window.addEventListener('mousemove', (event) => {
+      tgX = event.clientX;
+      tgY = event.clientY;
+    });
+
+    move();
+  });
+
   return (
     <div className="w-full h-[90vh] h-[90svh] md:h-screen relative" id="header">
       <div className="absolute heading-container">
@@ -18,7 +44,34 @@ const Header = () => {
         </p>
       </div>
       {/* <div className=" absolute bg-black/15 w-full h-full"></div> */}
-      <div></div>
+      <div className="gradient-bg">
+        <svg xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <filter id="goo">
+              <feGaussianBlur
+                in="SourceGraphic"
+                stdDeviation="10"
+                result="blur"
+              />
+              <feColorMatrix
+                in="blur"
+                mode="matrix"
+                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                result="goo"
+              />
+              <feBlend in="SourceGraphic" in2="goo" />
+            </filter>
+          </defs>
+        </svg>
+        <div className="gradients-container">
+          <div className="g1"></div>
+          <div className="g2"></div>
+          <div className="g3"></div>
+          <div className="g4"></div>
+          <div className="g5"></div>
+          <div className="interactive"></div>
+        </div>
+      </div>
       {/* <Spline scene="https://prod.spline.design/D726QFNsxJlXYBml/scene.splinecode" /> */}
     </div>
   );
