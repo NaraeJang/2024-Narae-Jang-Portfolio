d56dc7d71fce24b0f82200add35b90c0147e7b66
NaraeJang <j.naaaarae@gmail.com>
2024-03-13 17:39:32 -0400

    Complete Gana Design System

--- src/components/pages/gamadara/GanaDesignSystem.jsx
+++ src/components/pages/gamadara/GanaDesignSystem.jsx
@@ -1,4 +1,19 @@
+import { GanaTitleText } from './';
+import designSystemImg from '../../../assets/ganadara-assets/Design-System.webp';
+
 const GanaDesignSystem = () => {
-  return <div>GanaDesignSystem</div>;
+  return (
+    <section id="gana-design-system">
+      <div className="custom-container w-[90%] text-left">
+        <GanaTitleText text="Design system" />
+        <img
+          data-enlargable
+          src={designSystemImg}
+          alt="Ganadara design system img"
+          className="mx-auto w-full max-w-[70rem] mt-6"
+        />
+      </div>
+    </section>
+  );
 };
 export default GanaDesignSystem;
