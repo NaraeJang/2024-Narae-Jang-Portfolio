7c3fed60c0ee897798cbc3186dddb71e78783818
NaraeJang <j.naaaarae@gmail.com>
2024-03-14 17:21:28 -0400

    complete ending

--- src/components/pages/RedesignComponents/IflyEnding.jsx
+++ src/components/pages/RedesignComponents/IflyEnding.jsx
@@ -1,4 +1,45 @@
+import TitleText from '../TitleText';
+
 const IflyEnding = () => {
-  return <div>IflyEnding</div>;
+  return (
+    <section>
+      <div className="flex flex-col md:flex-row md:gap-10">
+        <div className="custom-container w-[90%]">
+          <TitleText text="Result" />
+          <p>
+            Current IFLY website uses most of the new features we built. As a
+            result, it enables users to browse and shop all items without
+            anxiety to find several websites. In addition, Calego was able to
+            integrate all websites into one, drive more sales and gather SEO and
+            reviews.
+          </p>
+        </div>
+
+        <div className="custom-container w-[90%]">
+          <TitleText text="Takeaways" />
+          <p>
+            <strong className="text-gray-600">Less is more.</strong>
+            <br /> When Calego had 4 different webistes, it overwhelmed the
+            users. By simplifying the front end of the website we facilitated
+            the user journey.
+          </p>
+          <p className="mt-4">
+            <strong className="text-gray-600">
+              Collaborative work always leads to better result.
+            </strong>
+            <br /> As a team, we communticated and brainstormed togther so we
+            could get diverse ideas to solve the problems that we had. If I had
+            done this project alone, I would not have been able to reach the
+            effective solution.
+          </p>
+          <p className="mt-4 mb-[10rem]">
+            <strong className="text-gray-600">Divide and conquer!</strong>
+            <br /> when the project is too big to handle, breaking it into
+            pieces and solving them one by one helps alleviate development.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 export default IflyEnding;
