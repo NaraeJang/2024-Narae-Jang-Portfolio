6b036b45f5e15000ef13874ff0e869fc1b75276f
NaraeJang <j.naaaarae@gmail.com>
2024-03-14 11:55:24 -0400

    complete Ganadara

--- src/components/pages/gamadara/GanaBookingFlow.jsx
+++ src/components/pages/gamadara/GanaBookingFlow.jsx
@@ -8,36 +8,36 @@ import bookingFlowImg05 from '../../../assets/ganadara-assets/BookingFlow05.webp
 const GanaBookingFlow = () => {
   return (
     <section id="gana-booking-flow" className="bg-[#f5f5f5]">
-      <div className="custom-container text-left w-[90%]">
+      <div className="custom-container text-left w-[90%] ">
         <GanaTitleText text="Booking flow" />
 
-        <div className="flex flex-col gap-8 md:grid md:grid-cols-6 md:gap-4 md:gap-y-8">
-          <div className="flex flex-col gap-5 md:col-span-2">
+        <div className="flex flex-col gap-8 md:grid md:grid-cols-6 md:gap-4 md:gap-y-8 lg:flex lg:flex-row lg:flex-wrap lg:gap-8 lg:justify-between lg:max-w-[1370px] lg:ml-auto lg:mr-auto">
+          <div className="flex flex-col gap-5 md:col-span-2 lg:col-span-3 ">
             <img
               src={bookingFlowImg01}
               alt="gana booking flow img"
-              className="h-[26rem] mx-auto md:mr-auto"
+              className="h-[26rem] mx-auto md:mr-auto md:ml-0"
             />
-            <p className="md:max-w-[13rem]">
+            <p className="lg:max-w-[13rem]">
               On the home page, a user can{' '}
               <strong>select a date, time and number of their group.</strong>
             </p>
           </div>
 
-          <div className="flex flex-col gap-5 md:col-span-4">
+          <div className="flex flex-col gap-5 md:col-span-4 lg:col-span-6 ">
             <div className="flex flex-col gap-5 md:flex-row md:gap-2 md:justify-between">
               <img
                 src={bookingFlowImg02}
                 alt="gana booking flow img"
-                className="h-[26rem] mx-auto"
+                className="h-[26rem] mx-auto md:mr-auto md:ml-0"
               />
               <img
                 src={bookingFlowImg03}
                 alt="gana booking flow img"
-                className="h-[26rem] mx-auto"
+                className="h-[26rem] mx-auto md:mr-auto md:ml-0"
               />
             </div>
-            <p className="md:pl-4">
+            <p className="md:pl-4 lg:max-w-[25rem]">
               For the <strong>new user,</strong> the app will require the
               <strong>user&apos;s phone number to verify.</strong> The user also
               can skip if they don&apos;t want to continue with their phone
@@ -49,26 +49,26 @@ const GanaBookingFlow = () => {
             </p>
           </div>
 
-          <div className="flex flex-col gap-5 md:col-span-2">
+          <div className="flex flex-col gap-5 md:col-span-2 lg:col-span-3 ">
             <img
               src={bookingFlowImg04}
               alt="gana booking flow img"
-              className="h-[26rem] mx-auto md:mr-auto"
+              className="h-[26rem] mx-auto md:mr-auto md:ml-0"
             />
-            <p className="md:max-w-[13rem]">
+            <p className="lg:max-w-[13rem]">
               Before the user makes a reservation, the app will{' '}
               <strong>show the confirmation page to the user</strong> to check
               if all information is correct.
             </p>
           </div>
 
-          <div className="flex flex-col gap-5 md:col-span-4">
+          <div className="flex flex-col gap-5 md:col-span-4 ">
             <img
               src={bookingFlowImg05}
               alt="gana booking flow img"
-              className="h-[26rem] mx-auto md:mr-auto"
+              className="h-[26rem] mx-auto md:mr-auto md:ml-0"
             />
-            <p className="md:max-w-[13rem]">
+            <p className="md:max-w-[20rem] lg:max-w-[13rem]">
               Once the user completes the booking, the app{' '}
               <strong>
                 offers options to the user for their next journey.
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // Simple 16 column grid
+        13: 'repeat(13, minmax(0, 1fr))',
+      },
+    },
   },
   plugins: [],
 };
