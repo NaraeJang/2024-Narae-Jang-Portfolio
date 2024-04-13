a0c342ce962eeb92ee67206facaa486ae7e81dc7
NaraeJang <j.naaaarae@gmail.com>
2024-04-13 01:20:39 -0400

    about page done

--- src/components/newHompage/HomeAboutExperience.jsx
+++ src/components/newHompage/HomeAboutExperience.jsx
@@ -13,25 +13,44 @@ const HomeAboutExperience = () => {
       scrollTrigger: {
         trigger: '.home_about--experience',
         toggleActions: 'restart reset restart reset',
-        start: 'top 80%',
-        markers: true,
+        start: 'top 90%',
+        markers: false,
       },
+    });
+
+    tl.to('.work-title', {
+      y: 0,
+      opacity: 1,
+      duration: 0.5,
+    });
+
+    tl.to('.work-job', {
+      y: '0px',
+      opacity: 1,
+      stagger: 0.3,
+      duration: 0.5,
+    });
+
+    tl.to('.work-line', {
+      x: 0,
+      opacity: 1,
+      duration: 0.5,
     });
   }, []);
 
   return (
     <div className="home_about--experience px-[2vw] mt-20">
-      <div className="flex flex-row justify-between items-center">
+      <div className="work-title flex flex-row justify-between items-center">
         <h5 className="dt-black text-primary-600 text-2xl lg:text-3xl">
           Past Experience
         </h5>
         <p className="text-primary-500">2018 - {currentYear}</p>
       </div>
-      <hr className="my-8" />
+      <hr className="work-line my-8" />
       {workExperience.map((work) => {
         return (
           <div key={work.id}>
-            <div className="flex flex-col md:flex-row justify-between items-center ">
+            <div className="work-job flex flex-col md:flex-row justify-between items-center ">
               <div className="flex flex-row justify-between  w-full md:w-[35vw] items-center">
                 <p className="min-w-[10rem] text-left text-primary-500">
                   {work.year}
@@ -46,7 +65,7 @@ const HomeAboutExperience = () => {
                 </p>
               </div>
             </div>
-            <hr className="my-8" />
+            <hr className="work-line my-8" />
           </div>
         );
       })}
