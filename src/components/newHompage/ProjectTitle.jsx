1c089bad79bbcd9dad4770ace6e5adfb1833ba8d
NaraeJang <j.naaaarae@gmail.com>
2024-04-18 11:06:08 -0400

    update

--- src/components/newHompage/ProjectTitle.jsx
+++ src/components/newHompage/ProjectTitle.jsx
@@ -1,12 +1,12 @@
-import Img01 from '../../assets/project-title-img/ProjectTitleImg.webp';
-import Img02 from '../../assets/project-title-img/ProjectTitleImg-1.webp';
-import Img03 from '../../assets/project-title-img/ProjectTitleImg-2.webp';
-import Img04 from '../../assets/project-title-img/ProjectTitleImg-3.webp';
-import Img05 from '../../assets/project-title-img/ProjectTitleImg-4.webp';
-import Img06 from '../../assets/project-title-img/ProjectTitleImg-5.webp';
-import Img07 from '../../assets/project-title-img/ProjectTitleImg-6.webp';
-import Img08 from '../../assets/project-title-img/ProjectTitleImg-7.webp';
-import Img09 from '../../assets/project-title-img/ProjectTitleImg-8.webp';
+// import Img01 from '../../assets/project-title-img/ProjectTitleImg.webp';
+// import Img02 from '../../assets/project-title-img/ProjectTitleImg-1.webp';
+// import Img03 from '../../assets/project-title-img/ProjectTitleImg-2.webp';
+// import Img04 from '../../assets/project-title-img/ProjectTitleImg-3.webp';
+// import Img05 from '../../assets/project-title-img/ProjectTitleImg-4.webp';
+// import Img06 from '../../assets/project-title-img/ProjectTitleImg-5.webp';
+// import Img07 from '../../assets/project-title-img/ProjectTitleImg-6.webp';
+// import Img08 from '../../assets/project-title-img/ProjectTitleImg-7.webp';
+// import Img09 from '../../assets/project-title-img/ProjectTitleImg-8.webp';
 
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -18,7 +18,7 @@ const Items = () => {
   useEffect(() => {
     const tl = gsap.timeline({
       scrollTrigger: {
-        trigger: '.project_title--container',
+        trigger: '#project-gallery',
         toggleActions: 'restart none none reverse',
         markers: false,
         scrub: true,
@@ -47,53 +47,63 @@ const Items = () => {
       </div>
 
       <div className="project_title--container h-[150vh] absolute">
-        <img
-          src={Img06}
-          alt="project cover photo"
-          className="project_title--img"
-        />
-        <img
-          src={Img07}
-          alt="project cover photo"
-          className="project_title--img"
-        />
-        <img
-          src={Img08}
-          alt="project cover photo"
-          className="project_title--img"
-        />
-        <img
-          src={Img04}
-          alt="project cover photo"
-          className="project_title--img"
-        />
-        <img
-          src={Img05}
-          alt="project cover photo"
-          className="project_title--img"
-        />
-        <img
-          src={Img09}
-          alt="project cover photo"
-          className="project_title--img"
-        />
-        <img
-          src={Img01}
-          alt="project cover photo"
-          className="project_title--img"
-        />
-        <img
-          src={Img02}
-          alt="project cover photo"
-          className="project_title--img"
-        />
-        <img
-          src={Img03}
-          alt="project cover photo"
-          className="project_title--img"
-        />
+        <div className="project_title--img"></div>
+        <div className="project_title--img"></div>
+        <div className="project_title--img"></div>
+        <div className="project_title--img"></div>
+        <div className="project_title--img"></div>
+        <div className="project_title--img"></div>
+        <div className="project_title--img"></div>
+        <div className="project_title--img"></div>
+        <div className="project_title--img"></div>
       </div>
     </section>
   );
 };
 export default Items;
+
+//  <img
+//           src={Img06}
+//           alt="project cover photo"
+//           className="project_title--img"
+//         />
+//         <img
+//           src={Img07}
+//           alt="project cover photo"
+//           className="project_title--img"
+//         />
+//         <img
+//           src={Img08}
+//           alt="project cover photo"
+//           className="project_title--img"
+//         />
+//         <img
+//           src={Img04}
+//           alt="project cover photo"
+//           className="project_title--img"
+//         />
+//         <img
+//           src={Img05}
+//           alt="project cover photo"
+//           className="project_title--img"
+//         />
+//         <img
+//           src={Img09}
+//           alt="project cover photo"
+//           className="project_title--img"
+//         />
+//         <img
+//           src={Img01}
+//           alt="project cover photo"
+//           className="project_title--img"
+//         />
+//         <img
+//           src={Img02}
+//           alt="project cover photo"
+//           className="project_title--img"
+//         />
+//         <img
+//           src={Img03}
+//           alt="project cover photo"
+//           className="project_title--img"
+//         />
