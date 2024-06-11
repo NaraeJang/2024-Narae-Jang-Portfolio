8830ec40745f591e99edf9237caa2d2e4250d6e9
NaraeJang <j.naaaarae@gmail.com>
2024-06-11 16:56:35 -0400

    finally add circular revealing animation

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -7,48 +7,58 @@ gsap.registerPlugin(ScrollTrigger);
 const Hero = () => {
   // REVEALING ANIMATION
   useEffect(() => {
-    // const tl = gsap.timeline({
-    //   defaults: {
-    //     ease: 'none',
-    //   },
-    //   scrollTrigger: {
-    //     trigger: '#g-hero-container',
-    //     toggleActions: 'restart none reverse none',
-    //     pin: true,
-    //     start: 'top top',
-    //     end: '+=800',
-    //     scrub: 1,
-    //     markers: true,
-    //   },
-    // });
-    // tl.from('#g-hero-container #g-hero-hidden-element', {
-    //   clipPath: 'circle(0% at 50% 50%)',
-    //   stagger: 1,
-    // });
+    const tl = gsap.timeline({
+      defaults: {
+        ease: 'none',
+      },
+      scrollTrigger: {
+        trigger: '#g-pin',
+        toggleActions: 'restart none reverse reset',
+        start: 'top bottom-=25%',
+        end: 'bottom top+=25%',
+        scrub: 1,
+        markers: true,
+      },
+    });
+
+    tl.to('#g-hero-hidden-element', {
+      clipPath: 'circle(100% at 50% 50%)',
+      stagger: 1,
+    });
   }, []);
 
   return (
-    <section
-      id="g-hero-container"
-      className="grid grid-cols-[1fr] grid-rows-[1fr] gap-0 items-center justify-center bg-yellow-500">
-      <div className="h-svh grid-area-hero bg-green-500 text-black flex justify-center items-center w-full">
-        <div className="max-w-[900px]">
-          <h1>section 01</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Accusantium, illo!
-          </p>
+    <section id="g-hero-container" className="relative h-[200svh]">
+      <div className="g-element-container absolute top-0 left-0 h-[200svh] w-full">
+        <div className="sticky top-0 left-0 h-svh flex justify-center items-center w-full">
+          <div className="max-w-3xl text-center">
+            <h1 className="max-w-xl mx-auto font-title uppercase text-[5rem] text-black leading-tight">
+              Transforming ideas into reality
+            </h1>
+            <p className="text-lg leading-relaxed">
+              My expertise encompasses branding, logo design, publication
+              design, digital marketing, UI/UX, and development, forging new
+              paths into the digital landscape.
+            </p>
+          </div>
+        </div>
+
+        <div id="g-pin" className="z-20">
+          {/* <p>element</p> */}
         </div>
       </div>
 
       <div
         id="g-hero-hidden-element"
-        className="h-svh grid-area-hero bg-blue-500 clip-path-hero text-black flex justify-center items-center w-full">
-        <div className="max-w-[900px]">
-          <h1>the new section 02</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Accusantium, illo!
+        className="sticky top-0 left-0 z-10 h-svh clip-path-hero flex justify-center items-center w-full bg-green-500 text-white">
+        <div className="max-w-3xl text-center">
+          <h1 className="max-w-xl mx-auto font-title uppercase text-[5rem] text-white leading-tight">
+            Transforming ideas into reality
+          </h1>
+          <p className="text-lg leading-relaxed">
+            My expertise encompasses branding, logo design, publication design,
+            digital marketing, UI/UX, and development, forging new paths into
+            the digital landscape.
           </p>
         </div>
       </div>
