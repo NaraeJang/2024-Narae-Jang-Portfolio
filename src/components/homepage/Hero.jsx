82da6c3e3f36846e85f827daa377e75e0358c26d
NaraeJang <j.naaaarae@gmail.com>
2024-06-12 13:29:51 -0400

    update hero section: fully responsive

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -41,7 +41,7 @@ const Hero = () => {
             <h1 className="w-full md:max-w-xl mx-auto font-title uppercase text-5xl md:text-[5rem] text-black leading-tight">
               Transforming ideas into reality
             </h1>
-            <p className="text-lg leading-relaxed">
+            <p className="text-base md:text-lg leading-relaxed">
               My expertise encompasses branding, logo design, publication
               design, digital marketing, UI/UX, and development, forging new
               paths into the digital landscape.
@@ -57,11 +57,11 @@ const Hero = () => {
       <div
         id="g-hero-hidden-element"
         className="sticky top-0 left-0 z-10 h-svh clip-path-hero flex justify-center items-center w-svw bg-black text-white">
-        <div className="max-w-3xl text-center z-10">
-          <h1 className="max-w-xl mx-auto font-title uppercase text-[5rem] text-white leading-tight">
+        <div className="w-full md:max-w-3xl text-center px-4 z-10">
+          <h1 className="w-full md:max-w-xl mx-auto font-title uppercase text-5xl md:text-[5rem] text-white leading-tight">
             Transforming ideas into reality
           </h1>
-          <p className="text-lg leading-relaxed">
+          <p className="text-base md:text-lg leading-relaxed">
             My expertise encompasses branding, logo design, publication design,
             digital marketing, UI/UX, and development, forging new paths into
             the digital landscape.
@@ -80,7 +80,7 @@ const Hero = () => {
           }}>
           <ShaderGradient
             control="query"
-            urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=3.5&cAzimuthAngle=180&cDistance=3.6&cPolarAngle=90&cameraZoom=1&color1=%23ff6a00&color2=%23dbba95&color3=%23adb2e1&destination=onCanvas&embedMode=off&envPreset=lobby&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=on&lightType=3d&pixelDensity=1&positionX=-1.4&positionY=0&positionZ=0&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=0&rotationY=10&rotationZ=50&shader=defaults&toggleAxis=false&type=waterPlane&uAmplitude=0&uDensity=1.3&uFrequency=5.5&uSpeed=0.4&uStrength=3.8&uTime=0&wireframe=false&zoomOut=false"
+            urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=3.95&cAzimuthAngle=170&cDistance=4.4&cPolarAngle=70&cameraZoom=1&color1=c3e4ff&color2=eae2ff&color3=%23ffffff&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0.9&positionZ=-0.3&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=45&rotationY=0&rotationZ=0&shader=defaults&type=waterPlane&uAmplitude=0&uDensity=1.2&uFrequency=0&uSpeed=0.2&uStrength=3.4&uTime=0&wireframe=false"
           />
         </ShaderGradientCanvas>
       </div>
