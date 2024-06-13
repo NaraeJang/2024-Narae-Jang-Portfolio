8050f08a7af4c6723b862cddfaaf404df436747a
NaraeJang <j.naaaarae@gmail.com>
2024-06-13 18:50:43 -0400

    update Jounrey btn

--- src/components/homepage/OtherSection/Journey.jsx
+++ src/components/homepage/OtherSection/Journey.jsx
@@ -10,19 +10,19 @@ const Journey = () => {
       <ol className="list-none mt-10 md:mt-20 md:px-[4%]">
         <li className="border-b border-b-neutral-700 lg:hover:border-b-neutral-800 lg:border-b-black grid grid-cols-3 lg:grid-cols-7 gap-4 text-neutral-400 pb-6">
           <p className="col-span-3">Current</p>
-          <div id="certification-btn" className="col-span-4">
-            <p className="text-neutral-100 text-lg">
+          <div className="col-span-4">
+            <p className="text-neutral-100 text-lg mb-4">
               Have been self-studying UX/UI design and front-end development.
               I’ve gotten 13 certifications in both fields. Always looking for
               opportunities to learn more technologies.
             </p>
-            <a href="#" className="group">
-              <div className="relative overflow-hidden border border-gray-800 text-gray-800 inline-block text-[15px] leading-[15px] py-[18px] px-[18px] bg-white cursor-pointer select-none">
-                <span className="relative z-10 transition-colors duration-[600ms] ease-[cubic-bezier(0.48,0,0.12,1)]">
-                  Hover me
+            <a href="#" id="certification-btn" className="group">
+              <div className="relative overflow-hidden border border-neutral-400 text-neutral-400 inline-block text-[15px] leading-[15px] py-3 px-6 rounded-full bg-transparent cursor-pointer select-none">
+                <span className="relative z-10 transition-colors duration-[300ms] ease-[cubic-bezier(0.48,0,0.12,1)]">
+                  View Certifications
                 </span>
-                <span className="text-white block absolute bottom-0 transition-all duration-[500ms] ease-[cubic-bezier(0.48,0,0.12,1)] z-20 opacity-0 top-1/2 left-1/2 transform translate-y-[225%] translate-x-[-50%] h-[14px] leading-[13px]">
-                  Thanks!
+                <span className="bg-white text-black w-full h-full grid place-items-center text-center absolute bottom-0 transition-all duration-[300ms] ease-[cubic-bezier(0.48,0,0.12,1)] z-20 opacity-0 top-1/2 left-1/2 transform translate-y-[225%] translate-x-[-50%] leading-[13px]">
+                  Design & Development
                 </span>
               </div>
             </a>
