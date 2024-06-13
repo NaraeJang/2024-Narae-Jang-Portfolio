1df2d3c430125f75aa91ea03c490ba4387dd18d3
NaraeJang <j.naaaarae@gmail.com>
2024-06-13 10:05:21 -0400

    giveing the effect in certification btn

--- src/components/homepage/OtherSection/Journey.jsx
+++ src/components/homepage/OtherSection/Journey.jsx
@@ -10,14 +10,21 @@ const Journey = () => {
       <ol className="list-none mt-10 md:mt-20 md:px-[4%]">
         <li className="border-b border-b-neutral-700 lg:hover:border-b-neutral-800 lg:border-b-black grid grid-cols-3 lg:grid-cols-7 gap-4 text-neutral-400 pb-6">
           <p className="col-span-3">Current</p>
-          <div className="col-span-4">
+          <div id="certification-btn" className="col-span-4">
             <p className="text-neutral-100 text-lg">
               Have been self-studying UX/UI design and front-end development.
               I’ve gotten 13 certifications in both fields. Always looking for
               opportunities to learn more technologies.
             </p>
-            <a href="#">
-              <p>See Certification</p>
+            <a href="#" className="group">
+              <div className="relative overflow-hidden border border-gray-800 text-gray-800 inline-block text-[15px] leading-[15px] py-[18px] px-[18px] bg-white cursor-pointer select-none">
+                <span className="relative z-10 transition-colors duration-[600ms] ease-[cubic-bezier(0.48,0,0.12,1)]">
+                  Hover me
+                </span>
+                <span className="text-white block absolute bottom-0 transition-all duration-[500ms] ease-[cubic-bezier(0.48,0,0.12,1)] z-20 opacity-0 top-1/2 left-1/2 transform translate-y-[225%] translate-x-[-50%] h-[14px] leading-[13px]">
+                  Thanks!
+                </span>
+              </div>
             </a>
           </div>
         </li>
