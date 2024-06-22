2551d5a10537cb00067d1fe36c954be5b247f7d4
NaraeJang <j.naaaarae@gmail.com>
2024-06-21 22:35:12 -0400

    test hero js

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -85,9 +85,17 @@ const Hero = () => {
             left: 0,
             pointerEvents: 'none',
           }}>
-          <ShaderGradient
+          {/* <ShaderGradient
             control="query"
             urlString="https://www.shadergradient.co/customize?animate=on&axesHelper=off&bgColor1=%23000000&bgColor2=%23000000&brightness=3.95&cAzimuthAngle=170&cDistance=4.4&cPolarAngle=70&cameraZoom=1&color1=c3e4ff&color2=eae2ff&color3=%23ffffff&destination=onCanvas&embedMode=off&envPreset=city&format=gif&fov=45&frameRate=10&gizmoHelper=hide&grain=off&lightType=3d&pixelDensity=1&positionX=0&positionY=0.9&positionZ=-0.3&range=enabled&rangeEnd=40&rangeStart=0&reflection=0.1&rotationX=45&rotationY=0&rotationZ=0&shader=defaults&type=waterPlane&uAmplitude=0&uDensity=1.2&uFrequency=0&uSpeed=0.2&uStrength=3.4&uTime=0&wireframe=false"
+          /> */}
+          <ShaderGradient
+            animate="on"
+            type="sphere"
+            color1="#c3e4ff"
+            color2="#6ec3f4"
+            color3="#b9beff"
+            brightness={3.5}
           />
         </ShaderGradientCanvas>
       </div>
