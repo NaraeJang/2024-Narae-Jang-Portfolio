57af722be99a598e38d0c02bc81b784edbc61a32
NaraeJang <j.naaaarae@gmail.com>
2024-07-16 14:15:54 -0400

    test01: add multiple swipers in one page

--- src/components/homepage/HeroSection/GridDownTime.jsx
+++ src/components/homepage/HeroSection/GridDownTime.jsx
@@ -1,7 +1,54 @@
+// SWIPER
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/autoplay';
+import { Pagination, Navigation, Autoplay, EffectFade } from 'swiper/modules';
+
 const GridDownTime = () => {
   return (
-    <div className="hidden invisible md:block md:visible md:col-span-5 md:row-span-4 bg-green-200 rounded-2xl">
-      5
+    <div className="hidden invisible md:block md:visible md:col-span-5 md:row-span-4 rounded-2xl overflow-hidden">
+      <div className="w-full h-full">
+        <Swiper
+          modules={[Navigation, Autoplay, EffectFade]}
+          effect="fade"
+          fadeEffect={{ crossFade: true }}
+          navigation={false}
+          pagination={{
+            type: 'progressbar',
+          }}
+          style={{
+            '--swiper-navigation-size': '1rem',
+            '--swiper-navigation-color': '#fefefe',
+          }}
+          autoplay={{ delay: 2500, pauseOnMouseEnter: true }}
+          speed="500"
+          loop={true}
+          className="mySwiper2">
+          <SwiperSlide>
+            {({ isActive }) => (
+              <div className="bg-blue-500 w-full h-full text-white">
+                In My DownTime, I enjoy {isActive ? 'camping' : 'not active'}
+              </div>
+            )}
+          </SwiperSlide>
+          <SwiperSlide>
+            {({ isActive }) => (
+              <div className="bg-yellow-500 w-full h-full text-white">
+                In My DownTime, I enjoy {isActive ? 'hiking' : 'not active'}
+              </div>
+            )}
+          </SwiperSlide>{' '}
+          <SwiperSlide>
+            {({ isActive }) => (
+              <div className="bg-red-500 w-full h-full text-white">
+                In My DownTime, I enjoy {isActive ? 'drawing' : 'not active'}
+              </div>
+            )}
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 };
