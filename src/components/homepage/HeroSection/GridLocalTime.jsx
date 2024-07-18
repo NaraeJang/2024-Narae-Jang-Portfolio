fcdb18b7c42b31c7a9acac50d21d90c829252c7c
NaraeJang <j.naaaarae@gmail.com>
2024-07-17 22:36:46 -0400

    removed date in GridLocalDate

--- src/components/homepage/HeroSection/GridLocalTime.jsx
+++ src/components/homepage/HeroSection/GridLocalTime.jsx
@@ -20,9 +20,9 @@ const GridLocalTime = () => {
   }, []);
 
   const hourlyTimeFormat = time.format('h:mm a');
-  const dateFormat = time.format('MMM DD YYYY');
-  const hour = time.hour();
-  //  const hour = 12;
+  const dateFormat = time.format('MMM DD');
+  // const hour = time.hour();
+  const hour = 12;
 
   // Determine if the current time is between 7 PM and 6 AM
   const isNight = hour >= 19 || hour < 6;
@@ -34,26 +34,26 @@ const GridLocalTime = () => {
           ? 'local-container-night text-neutral-50'
           : 'local-container-day'
       } overflow-hidden`}>
-      <div className="absolute p-6 flex flex-col gap-0 items-start">
+      <div className="absolute p-6 flex flex-row justify-between gap-0 w-full">
         <p
-          className={`text-lg leading-none uppercase font-title font-medium z-10 ${
+          className={`text-lg leading-none uppercase font-title z-10 ${
             isNight ? `text-neutral-50` : `text-[#f59758]`
           }`}>
           montreal
-        </p>
+        </p>{' '}
+        {/* <p
+          className={`text-lg leading-none uppercase font-title z-10 ${
+            isNight ? `text-neutral-50` : `text-[#f59758]`
+          }`}>
+          {dateFormat}
+        </p> */}
       </div>
-      <div className="grid place-items-center w-full h-full mt-2">
+      <div className="grid place-items-center w-full h-full">
         <div
           className={`aspect-square mx-auto h-full max-h-[80%] rounded-full text-center grid place-items-center ${
             isNight ? 'local-circle-night' : 'local-circle-day'
           }`}>
-          <div className="flex flex-col gap-2 p-4 -mt-4">
-            <p
-              className={`time-subtitle font-normal  ${
-                isNight ? `text-neutral-500` : `text-neutral-50`
-              }`}>
-              {dateFormat}
-            </p>
+          <div className="flex flex-col gap-2 p-4">
             <p className="time-main font-title tracking-tight">
               {hourlyTimeFormat}
             </p>
